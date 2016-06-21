8a3c302cc165c48af45cac0e0bf15b0b707829a8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-21 22:18:50 +0100

    Added Visited property

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var initial_cities = require('./initial_cities');
 
+var cities = initial_cities.map(function(city) {
+    city.Visited = city.City.length < 8;
+    return city;
+});
+
 var port = process.env.PORT || 3000;
 
 var app = express();
@@ -14,7 +19,7 @@ app.use('/api/cities', apiRouter);
 
 function handleGet(req, res, _) {
     console.log('handleGet');
-    res.json(initial_cities);
+    res.json(cities);
 }
 
 app.listen(port, function () {
