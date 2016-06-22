ee7d246b2272f3b4d07ef07e239fa00a5b923938
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-22 16:09:24 +0100

    Moved some files around

--- client/js/httpBackendSetup.js
+++ client/js/httpBackendSetup.js
@@ -18,11 +18,12 @@ function httpBackendSetup($httpBackend) {
     $httpBackend.whenGET(URL_REGEXP_FOR_ALL_HTML_FILES).passThrough();
 
     $httpBackend.whenGET('api/cities').respond(cities);
+
     $httpBackend.whenPOST('api/cities').respond(function(method, url, data) {
         const city = angular.fromJson(data);
         city.Visited = false;
+        console.log(`whenPOST('${url}'): adding ${JSON.stringify(city)}`);
         cities.push(city);
-        console.log(`whenPOST('api/cities'): adding ${JSON.stringify(city)}`);
         return [200, city, {}];
     });
 }
